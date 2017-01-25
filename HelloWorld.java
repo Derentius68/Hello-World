50e681137ca838523451bb97c8733e9973ac8f12
Derentius68 <asbywater@gmail.com>
2017-01-25 13:37:01 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Github link: https://github.com/Derentius68/Hello-World
  */
 package hello.world;
 
 /**
- *
  * @author Drew
  */
 public class HelloWorld 
